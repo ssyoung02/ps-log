a7257c826369b0a1f6120b7cb9b7c64e7234b634
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-09-11 14:50:42 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,22 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
+        int row = clothes.length;
         
         HashMap<String, Integer> map = new HashMap<>();
         
-        int row = clothes.length;
-        
-        for (int i = 0; i < row; i++){ // 의상 종류별로 count
+        for (int i = 0; i < row; i++) {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        Iterator<String> keys = map.keySet().iterator();
+        Iterator<String> keySet = map.keySet().iterator();
         
-        while(keys.hasNext()){
-            String key = keys.next();
-            answer *= map.get(key) + 1; // 의상 개수 + 착용하지 않은 경우
+        while (keySet.hasNext()) {
+            String key = keySet.next();
+            answer *= map.get(key) + 1;
         }
-        answer -= 1; // 전부 착용하지 않은 경우
+        answer -= 1;
         
         return answer;
     }
