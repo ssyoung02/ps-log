1979f0fb82e3a39650506c66c848f709e5767013
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-09-11 13:12:17 +0900

    [level 1] Title: 폰켓몬, Time: 8.99 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+        HashMap <Integer, Integer> map = new HashMap<>();
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (Integer num : nums){
-            map.put(num, map.getOrDefault(num,0) + 1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(map.get(num), 0) + 1);
         }
         
-        
-        if (map.size() > nums.length / 2) { //폰켓몬 종류가 N/2보다 많을 때
+        if (map.size() > nums.length / 2) {
             answer = nums.length / 2;
         } else {
             answer = map.size();
         }
-        
         return answer;
     }
 }
