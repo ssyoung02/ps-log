34803ed6834a1c04f5303a95dc63ea595e64a295
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-09-18 13:25:25 +0900

    [level 2] Title: 올바른 괄호, Time: 20.24 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,17 +5,16 @@ class Solution {
         boolean answer = true;
 
         Stack<Character> stack = new Stack<>();
-       
+        
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(') {    
-                stack.push(c);                 
+            if (c == '(') {
+                stack.push(c);
             } else {
-                if (stack.isEmpty()) {
-                    answer = false;
-                    break;
-                } else {
+                if (!stack.isEmpty()) {
                     stack.pop();
+                } else {
+                    return false;
                 }
             }
         }
