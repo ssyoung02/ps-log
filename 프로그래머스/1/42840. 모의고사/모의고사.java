7a8bbef9b18b1d556bf86c30e2d27d11472a7f69
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-09-29 22:52:54 +0900

    [level 1] Title: 모의고사, Time: 1.04 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        ArrayList<Integer> answerArr = new ArrayList<>();
+        
+        int correct1 = 0;
+        int correct2 = 0;
+        int correct3 = 0;
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        
+        for (int i = 0; i < answers.length; i++) {
+            int index1 = i % 5;
+            int index2 = i % 8;
+            int index3 = i % 10;
+            if (answers[i] == one[index1]) {
+                correct1++;
+            }
+            if (answers[i] == two[index2]) {
+                correct2++;
+            }
+            if (answers[i] == three[index3]) {
+                correct3++;
+            }
+        }
+        
+        int max = Math.max(correct1, Math.max(correct2, correct3));
+        
+        if (correct1 == max) {
+            answerArr.add(1);
+        }
+        if (correct2 == max) {
+            answerArr.add(2);
+        }
+        if (correct3 == max) {
+            answerArr.add(3);
+        }
+        int[] answer = new int[answerArr.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerArr.get(i);
+        }
+        return answer;
+    }
+}
