a1f370900f64af53676260820e0b76ebb12b2b3b
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-10-02 14:32:54 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,18 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        for (int i = 1; i <= (int)Math.sqrt(yellow); i++) {
-            if (yellow % i == 0) {
-                int x = yellow / i;
-                if (2 * (x + i) + 4 == brown) {
+        for (int y = 1; y <= (int)Math.sqrt(yellow); y++) {
+            if (yellow % y == 0) {
+                int x = yellow / y;
+                if ((x + y) * 2 + 4 == brown) {
                     answer[0] = x + 2;
-                    answer[1] = i + 2;
+                    answer[1] = y + 2;
                     break;
                 }
             }
         }
-        
         return answer;
     }
 }
