6294fff26bb0e9c5acbf3a28b0c60a7389140706
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-10-02 13:41:16 +0900

    [level 2] Title: 피로도, Time: 3.63 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,24 @@
+import java.util.*;
+
 class Solution {
-    public int maxCount = 0; 
-    public boolean[] visited;
-
+    boolean[] visited;
+    int answer = 0;
+    
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        visited= new boolean[dungeons.length];
         dfs(k, 0, dungeons);
+        return answer;
+    }
+    private void dfs(int current, int dungeonCount, int[][] dungeons) {
+        answer = Math.max(answer, dungeonCount);
         
-        return maxCount;
-    }
-
-    public void dfs(int current, int dungeonCount, int[][] dungeons) {
-        maxCount = Math.max(maxCount, dungeonCount);
-
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && current >= dungeons[i][0]) {
-            
-                visited[i] = true;
-                dfs(current - dungeons[i][1], dungeonCount + 1, dungeons);
-                
-                visited[i] = false;
-            }
+              if (!visited[i] && current >= dungeons[i][0]) {
+                  visited[i] = true;
+                  dfs(current - dungeons[i][1], dungeonCount + 1, dungeons);
+                  visited[i] = false;
+              }
         }
+      
     }
-
 }
