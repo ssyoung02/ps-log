6fe9c7db9559d9b2686b70af7a2487c3f74ff92b
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-10-29 20:53:41 +0900

    Update 별 찍기 － 17.java

--- 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -7,32 +7,23 @@ class Main {
         int n = sc.nextInt();
         StringBuilder sb = new StringBuilder();
 
-        // 첫번째
-        for (int i = 1; i < n; i++) {
-            sb.append(" ");
-        }
-        if (n > 1) {
-            sb.append("*");
-            sb.append("\n");   
-        }
-       
-        for (int i = 2; i < n; i++) {
+        
+        for (int i = 1; i <= n; i++) {
+            // 왼쪽 공백
             for (int j = i; j < n; j++) {
                 sb.append(" ");
             }
-            sb.append("*");
-    
-            for (int j = 0; j < 2 * i - 3 ; j++) {
-                sb.append(" ");
+
+            for (int j = 1; j < 2 * i; j++) {
+                if (j == 1 || j == 2 * i - 1 || i == n) {
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
             }
-            sb.append("*");
             sb.append("\n");
         }
-
-        // 마지막 줄
-        for (int i = 1; i < 2 * n; i++) {
-            sb.append("*");
-        }
-        System.out.print(sb.toString());
+        
+        System.out.print(sb);
     }
 }
