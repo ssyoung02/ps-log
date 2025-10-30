3ad5b56c4589cebb6f0e7eb451d1355f9de67aff
Seoyoung <81309514+ssyoung02@users.noreply.github.com>
2025-10-30 14:38:30 +0900

    [Bronze III] Title: A+B - 6, Time: 176 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -4,15 +4,13 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine();
-        String[] nums = new String[2];
-
+        int t = Integer.parseInt(sc.nextLine());
+        String[] str = new String[2];
         for (int i = 0; i < t; i++) {
-            String numStr = sc.nextLine();
-            nums = numStr.split(",");
-            int answer = Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
-            System.out.println(answer);
+            str = sc.nextLine().split(",");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            System.out.println(a+b);
         }
     }
 }
