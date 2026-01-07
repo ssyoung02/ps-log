b96ae7a7dcfbb2ab83f9056adc8656749de017ea
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-01-07 15:57:52 +0900

    [Bronze III] Title: 네 수, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        String ab = st.nextToken() + st.nextToken();
+        String cd = st.nextToken() + st.nextToken();
+  
+        long answer = Long.parseLong(ab) + Long.parseLong(cd);
+        System.out.println(answer);
+    }
+}
