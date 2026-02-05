5da9a2784d76d38e275c0793f313bf51e9b6e3f2
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-02-05 18:00:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 240 ms, Memory: 24184 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,68 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-	static int n,m,start;
-	static int[][] arr;
-	static boolean[] visit;
-	public static void reset() {
-		for (int i=1;i<n+1;i++) {
-			visit[i]=false;
-		}
-	}
-	public static void dfs(int i) {
-		visit[i]=true;
-		System.out.print(i+" ");
-		
-		for (int j=1;j<n+1;j++) {
-			if (visit[j]==false && arr[i][j]==1) {
-				dfs(j);
-			}
-		}
-	}
-	public static void bfs(int i) {
-		Queue <Integer> q = new LinkedList<>();
-		q.offer(i);
-		visit[i] = true;
-		
-		while (!q.isEmpty()) {
-			int temp = q.poll();
-			System.out.print(temp+" ");
-			for (int j=1;j<n+1;j++) {
-				if (visit[j]==false && arr[temp][j]==1) {
-					q.offer(j);
-					visit[j]=true;
-				}
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr=new int[n+1][n+1];
-		visit=new boolean[n+1];
-		start = Integer.parseInt(st.nextToken());
-		
-		for (int i=0;i<m;i++) {
-			StringTokenizer st1 = new StringTokenizer(bf.readLine());
-			int x = Integer.parseInt(st1.nextToken());
-			int y = Integer.parseInt(st1.nextToken());
-			arr[x][y]=1;
-			arr[y][x]=1;
-		}
-		
-		dfs(start);
-		System.out.println();
-		reset();
-		bfs(start);
-	}
+class Main {
 
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        dfs(v);
+        sb.append("\n");
+
+        visited = new boolean[n+1];
+        bfs(v);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start) {
+        visited[start] = true;
+        sb.append(start+ " ");
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[start][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }   
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            sb.append(temp + " ");
+
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[temp][i] == 1 && !visited[i]) {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
