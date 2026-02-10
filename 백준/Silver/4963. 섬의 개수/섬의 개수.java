61f3fc0e6adbe5e2820724bd648b1983cdce2a9d
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-02-11 00:46:35 +0900

    [Silver II] Title: 섬의 개수, Time: 152 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+    static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+    static int w, h;
+
+    static class Node {
+        int x, y;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int cnt = 0;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) break;
+
+            arr = new int[h+1][w+1];
+            visited = new boolean[h+1][w+1];
+
+            for (int i = 1; i <= h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= w; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 1; i <= h; i++) {
+                for (int j = 1; j <= w; j++) {
+                    if (arr[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 8; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx < 0 || cy < 0 || cx > h || cy > w || arr[cx][cy] == 0 || visited[cx][cy]) 
+                continue;
+            dfs(cx, cy);
+        }
+    }
+}
