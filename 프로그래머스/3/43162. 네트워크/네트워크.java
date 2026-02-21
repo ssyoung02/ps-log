26238dbd165f688e25b2ae64dbb4cbb359967748
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-02-22 02:06:06 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,24 @@
 class Solution {
+    boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int cnt = 0;
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 cnt++;
-                dfs(i, computers, visited);
+                dfs(i, computers);
             }
         }
         return cnt;
     }
     
-    private void dfs(int current, int[][] computers, boolean[] visited) {
+    private void dfs(int current, int[][] computers) {
         visited[current] = true;
         for (int i = 0; i < computers.length; i++) {
             if (computers[current][i] == 1 && !visited[i]) {
-                dfs(i, computers, visited);
+                dfs(i, computers);
             }
         }
     }
