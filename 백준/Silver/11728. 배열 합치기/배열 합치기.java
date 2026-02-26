5c09babebbb11a9554d7bf11eb8b58a31f5f159d
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-02-26 23:52:27 +0900

    [Silver V] Title: 배열 합치기, Time: 1212 ms, Memory: 301080 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,45 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] a = new int[n];
-        int[] b = new int[m];
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
         int p1 = 0;
         int p2 = 0;
-
         StringBuilder sb = new StringBuilder();
 
         while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) {
-                sb.append(a[p1++]).append(" ");
+            if (arr1[p1] <= arr2[p2]) {
+                sb.append(arr1[p1++]).append(" ");
             } else {
-                sb.append(b[p2++]).append(" ");
+                sb.append(arr2[p2++]).append(" ");
             }
         }
-        while (p1 < n) 
-            sb.append(a[p1++]).append(" ");
 
-        while (p2 < m) 
-            sb.append(b[p2++]).append(" ");
+        while (p1 < n) {
+            sb.append(arr1[p1++]).append(" ");
+        }
+
+        while (p2 < m) {
+            sb.append(arr2[p2++]).append(" ");
+        }
 
         System.out.print(sb);
     }
