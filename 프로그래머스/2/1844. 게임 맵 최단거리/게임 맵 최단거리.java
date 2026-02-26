ff07d49008693d84ec8f1984307aaf1d1ed9b1c9
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-02-26 21:32:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.85 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     int n, m, answer = -1;
+    Queue<Node> q = new LinkedList<>();
     boolean[][] visited;
     
     int[] dx = {0, 0, -1, 1};
     int[] dy = {-1, 1, 0, 0};
-    Queue<Node> q;
     
     class Node {
         int x, y, dist;
@@ -15,37 +15,36 @@ class Solution {
             this.y = y;
             this.dist = dist;
         }
-    } 
+    }
     
     public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
         visited = new boolean[n][m];
-        int cnt = 1;
-        q = new LinkedList<>();
         
-        q.add(new Node(0, 0, 1));
+        visited[0][0] = true;
+        q.add(new Node(0, 0, 0));
         bfs(maps, 0, 0);
         
         return answer;
     }
     
-    public void bfs (int[][] maps, int x, int y) {
-        int dist = 0;
+    public void bfs(int[][] maps, int x, int y) {
         while (!q.isEmpty()) {
             Node node = q.poll();
             
             if (node.x == n-1 && node.y == m-1) {
-                answer = node.dist;
+                answer = node.dist + 1;
                 return;
             }
-            
-            for (int i = 0; i < 4; i++) {
+                        
+            for (int i = 0 ; i < 4; i++) {
                 int nx = node.x + dx[i];
                 int ny = node.y + dy[i];
-                dist = node.dist;
+                int dist  = node.dist;
                 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] ||  maps[nx][ny] == 0) continue;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || maps[nx][ny] == 0)
+                    continue;
                 
                 visited[nx][ny] = true;
                 q.add(new Node(nx, ny, dist + 1));
