03e9c460d871a1abd29057e6120095a35003231b
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-04-14 22:46:05 +0900

    [level 3] Title: 입국심사, Time: 94.14 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        Arrays.sort(times);
+        
+        long low = 1;
+        long high = (long)times[times.length - 1] * n;
+        
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long cnt = 0;
+            
+            for (int i = 0; i < times.length; i++) {
+                cnt += mid / times[i];
+            }
+            
+            if (cnt >= n) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
