cc764b5174322f4fbe3a527b84a4ed262cc83840
Seoyoung Cho <81309514+ssyoung02@users.noreply.github.com>
2026-07-03 22:51:13 +0900

    Update 카펫.java

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,14 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        
-        int x = (int) Math.sqrt(yellow);
-        int y;
-        
-        while (true) {
-            if (yellow % x == 0) {
-                y = yellow / x;
-                if (brown == 2 * x + 2 * y + 4) {
-                    break;
+    public int[] solution(int brown, int yellow) {        
+        for (int i = (int) Math.sqrt(yellow); i >= 1; i--) {
+            if (yellow % i == 0) {
+                int x = yellow / i;
+                if (brown == 2 * i + 2 * x + 4) {
+                    return new int[]{x + 2, i + 2};
                 }
             }
-            x--;
         }
-        
-        answer[0] = y + 2;
-        answer[1] = x + 2;
-
-        return answer;
+     
+        return new int[] {0, 0};
     }
 }
